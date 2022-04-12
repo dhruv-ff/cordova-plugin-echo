da87ea83148583ec3803db9540ae2ecc2e8dc290
dhruv-ff <dhruv.naik@fitterfly.in>
2022-04-12 16:37:27 +0530

    Bug fixes

--- www/Echo.js
+++ www/Echo.js
@@ -1,5 +1,19 @@
-window.echo = function (str, callback) {
+function Echo() {
+}
+
+Echo.prototype.show = function (message, callback) {
     cordova.exec(callback, function (err) {
         callback('Nothing to echo.');
-    }, "Echo", "echo", [str]);
+    }, "Echo", "echo", [message]);
 };
+
+Echo.install = function () {
+    if (!window.plugins) {
+        window.plugins = {};
+    }
+
+    window.plugins.echo = new Echo();
+    return window.plugins.echo;
+};
+
+cordova.addConstructor(Echo.install);
